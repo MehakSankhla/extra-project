15bb2c17b6f2c9a14eef84debb73307ea6dda11a
MehakSankhla <75704051+MehakSankhla@users.noreply.github.com>
2021-08-30 09:31:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function spanelem(){
+    window.alert("New Delhi is the capital of northern India after the Muslim invasion and also the political capital.  The Taj Mahal was built as a mausoleum for the emperor Shan Jahan’s wife.  He was considered the wealthiest man in the world at the time (early 17th century).The Taj Mahal is actually a complext not just one building, you should look around the entire complex which provides impressive views and structures.  Nearby is the Red Fort which is an extravagant palace of the foreign Mughals (Indian for Mongols) empire.")
+    }
+    
+    function spanelem1(){
+    window.alert("Founded in 1577 by Ram Das, Amritsar is an important hub of Sikh history and culture. The main attraction here is Harmandir Sahib, opened in 1604 and still often referred to as the Golden Temple for its beautiful gold decoration. The holiest of India's many Sikh shrines (it also attracts many Hindus and people of other faiths), the temple was built in a blend of Hindu and Islamic styles, its lower marble section featuring such flourishes as ornate inlaid floral and animal motifs, while the large golden dome represents a lotus flower, a symbol of purity to Sikhs.")
+    }
+    function spanelem2(){
+    window.alert("Entering through the Lahore Gate, visitors reach Chhatta Chowk, a 17th-century covered bazaar where items such as silks, jewelry, gems, and silverware can be purchased, along with souvenirs and food items. The Naubat Khana within the Red Fort once housed the musicians who played for the emperor, and its fine galleries still contain many interesting musical instruments such as kettledrums, gongs, and cymbals. Diwan-i-Am, the Hall of Public Audiences, where the emperor would receive his subjects, is also worth seeing for its stunning white marble.")
+    }
+    
+    
+    function spanelem3(){
+    window.alert("Completed in the 12th century, the beautiful Qutub Minar-India's tallest minaret and now a UNESCO Word Heritage Site-attracts many international visitors eager to climb to the top for its breathtaking views of the surrounding area.This ornate five-story tower rises more than 70 meters and is covered with intricate carvings featuring the history of Qutub along with inscriptions from the Koran. It's also notable for being constructed of a number of different types of stone (the first three stories are made of red sandstone, while the fourth and fifth stories were built with marble and sandstone).")
+    }
+    
+    
+    
+    
